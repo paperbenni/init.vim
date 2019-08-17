504ab11bbaecf2c04fc126418bb8cc819d16a56d
paperbenni <paperbenni@gmail.com>
2019-08-17 21:17:12 +0200

    hashbang

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
+#!/usr/bin/env bash
+
+echo "installing paperbenni's neovim"
+
 if ! [ -e ~/.local/share/nvim/site/autoload/plug.vim ]
 then
 	echo "Installing vim-plug"
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
 cd
 rm -rf .config/nvim
 mkdir .config/nvim
@@ -12,6 +17,7 @@ curl https://raw.githubusercontent.com/paperbenni/nvim/master/init.vim > .config
 
 if ! python3 -c "import neovim"
 then
+	sudo pip3 install neovim pynvim
 fi
 
 if ! python2 -c "import neovim"
